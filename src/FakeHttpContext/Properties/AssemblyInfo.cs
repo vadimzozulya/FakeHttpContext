d70be4dc0ddce82557d04fb1b647dd76fae77588
Darrell Tunnell <darrell.tunnell@googlemail.com>
2017-04-16 16:49:16 +0100

    Closes #6 - Sign Assemblies.

--- src/FakeHttpContext/Properties/AssemblyInfo.cs
+++ src/FakeHttpContext/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: InternalsVisibleTo("FakeHttpContext.Tests")]
+[assembly: InternalsVisibleTo("FakeHttpContext.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f3b7c46cf95db1ec2210caf72318688eff1ad550cc1f04d6a9117c756fabb82d95de07998f8cab1f44ba26871692a4434d679fa755f075f336e73facc96a73df7a79293be57b61e1669c062a46cb03aac9351ff9317ddc7e05ccf43054e9b8e82afc094884df61ace061e8e8933ccbb0ca36a5a055f5e52f48674d2f4bace59e")]
 
 [assembly: AssemblyTitle("FakeHttpContext")]
 [assembly: AssemblyDescription("Unit testing utilite for simulate HttpContext.Current.")]
